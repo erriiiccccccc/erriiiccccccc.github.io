df8c32e65e3cf6cb5682cd02b0b37c308897f0d3
erriiiccccccc <ericng8766@gmail.com>
2023-08-22 15:19:07 +0800

    Update code

---
+++ js/script.js
@@ -0,0 +1,7 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","a Computer Science Student", "a Computer hardware enthusiast", "a hobbyist programmer", "a potential internship candidate"],
+    typeSpeed:100,
+    backSpeed:60,
+    loop:true
+})
