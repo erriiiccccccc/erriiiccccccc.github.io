1025730106f482eec249ce036fe90511b5a04270
erriiiccccccc <ericng8766@gmail.com>
2023-08-24 18:41:13 +0800

    update autoscroll

--- js/script.js
+++ js/script.js
@@ -34,3 +34,46 @@ var typed = new Typed(".typing",{
 
 //     setInterval(changeGreeting, 3999);
 // });
+
+/* ============================== button animation ============================ */
+var backToTopButton = document.getElementById("back-to-top-button");
+var isScrolling = false;
+
+function debounce(func, wait) {
+    let timeout;
+    return function() {
+        const context = this;
+        const args = arguments;
+        clearTimeout(timeout);
+        timeout = setTimeout(function() {
+            timeout = null;
+            func.apply(context, args);
+        }, wait);
+    };
+}
+
+function handleScroll() {
+    if (window.scrollY >= 200) {
+        backToTopButton.style.display = "block";
+        backToTopButton.style.animationName = "fadeIn";
+    } else {
+        backToTopButton.style.animationName = "fadeOut";
+        setTimeout(function() {
+            backToTopButton.style.display = "none";
+        }, 300);
+    }
+}
+
+window.addEventListener("scroll", debounce(function() {
+    if (!isScrolling) {
+        handleScroll();
+        isScrolling = true;
+        setTimeout(function() {
+            isScrolling = false;
+        }, 20); // Adjust the timeout value as needed
+    }
+}, 20)); // Adjust the debounce time as needed
+
+backToTopButton.addEventListener("click", function() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+});
